4a1654b5f961cfc88af02dd866c788c6c437be8a
mouaaaaaad <97707951+mouaaaaaad@users.noreply.github.com>
2022-10-20 15:26:15 +0200

    line

--- src/components/layouts/Line/index.jsx
+++ src/components/layouts/Line/index.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import styles from "./index.module.scss";
+import {BsGrid3X3GapFill} from 'react-icons/bs';
+
 
 const Index = () => {
-  return <div className={styles.quelquechose}></div>;
+  return <div className={styles.new}>
+    <h1 className={styles.text3} > Type a page title… </h1>
+    <div className={styles.icon}>
+    <BsGrid3X3GapFill/>
+    </div> 
+
+
+  </div>
+  
 };
 
 export default Index;
--- src/pages/acceuil/index.js
+++ src/pages/acceuil/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import TitlePage from "../../components/UI/Title/TitlePage";
 import Dashbord from "../../components/layouts/Dashbord";
 import Accueil from "../../components/layouts/Accueil";
-
+import Line from "../../components/layouts/Line"
 const Index = () => {
   return (
     <div>
       <form>
-        <Accueil />
+        
+        <Line />
       </form>
     </div>
   );
