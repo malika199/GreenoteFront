6ffead1ebb3106cb24a86dbc612232bac51cc0ee
kamy1997 <kamykechir@gmail.com>
2022-10-28 10:14:11 +0200

    editor

--- src/pages/wysiwing/index.js
+++ src/pages/wysiwing/index.js
@@ -1,14 +1,12 @@
 import React from "react";
-import Home from "../../components/layouts/Home";
-import {Dashboard} from '@material-ui/icons';
 import Editor from "../../components/Editor";
 
 const Index = () => {
 	return (
 			<div>
 				<form>
-				<Editor />
-					<Dashboard/>
+				
+					<Editor />
 					
 				</form>
 			</div>
